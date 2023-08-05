ae043a9e80efd7edb13fe1ef322576c92401a8c2
Mairon Costa <maironmscosta@gmail.com>
2023-08-05 14:51:35 -0300

    creating pwa test

--- app.js
+++ app.js
@@ -4,5 +4,41 @@ if ("serviceWorker" in navigator) {
             .register("/serviceWorker.js")
             .then(res => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
-    })
+    });
+}
+
+async function subscribeToPush() {
+    if (navigator.serviceWorker) {
+
+        const reg = await navigator.serviceWorker.getRegistration();
+
+        if (reg && reg.pushManager) {
+
+            const subscription = await reg.pushManager.getSubscription();
+
+            if (!subscription) {
+
+                // const key = await fetch("https://example.com/vapid_key");
+                // const keyData = await key.text();
+
+                const sub = await reg.pushManager.subscribe({
+                    applicationServerKey: "msc-push-notification",
+                    userVisibleOnly: true
+                });
+
+                await fetch("https://example.com/push_subscribe", {
+                    method: "POST",
+                    headers: {"Content-Type": "application/json"},
+                    body: JSON.stringify({
+                        endpoint: sub.endpoint,
+                        expirationTime: sub.expirationTime,
+                        keys: sub.toJSON().keys
+                    })
+                });
+
+            }
+
+        }
+
+    }
 }
